56cc076e750e653c7176842c2754513d4b55afb2
logonpf <logonpf@fiap.com.br>
2018-05-03 22:35:06 -0300

    JSF - Template, Validações e Navegação

--- 11-WS-JSF-Padaria-Client/src/br/com/fiap/bean/BebidaBean.java
+++ 11-WS-JSF-Padaria-Client/src/br/com/fiap/bean/BebidaBean.java
@@ -1,9 +1,13 @@
 package br.com.fiap.bean;
 
 import java.util.Calendar;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
 import br.com.fiap.ws.service.BebidaService;
 import br.com.fiap.ws.to.Bebida;
 
@@ -20,6 +24,52 @@ public class BebidaBean {
 		//Inicializar a data
 		bebida.setDataValidade(Calendar.getInstance());
 		service = new BebidaService();
+	}
+	
+	public List<Bebida> getBebidas(){
+		try {
+			return service.listar();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public String excluir(int codigo) {
+		FacesMessage msg;
+		try {
+			service.remover(codigo);
+			msg = new FacesMessage("Removido com sucesso!");
+		} catch (Exception e) {
+			e.printStackTrace();
+			msg = new FacesMessage("Erro");
+		}
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+		//Manter as mensagens após um redirect
+		FacesContext.getCurrentInstance().getExternalContext()
+			.getFlash().setKeepMessages(true);
+		return "lista-bebida?faces-redirect=true";
+	}
+	
+	public String salvar() {
+		FacesMessage msg;
+		try {
+			if (bebida.getCodigo() == 0) {
+				service.cadastrar(bebida);
+				msg = new FacesMessage("Cadastrado com sucesso!");
+			}else {
+				service.atualizar(bebida);
+				msg = new FacesMessage("Atualizado com sucesso!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			msg = new FacesMessage("Erro");
+		}
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+		//Manter as mensagens após um redirect
+		FacesContext.getCurrentInstance().getExternalContext()
+			.getFlash().setKeepMessages(true);
+		return "lista-bebida?faces-redirect=true"; //Navega para a página de listagem
 	}
 	
 	public Bebida getBebida() {
